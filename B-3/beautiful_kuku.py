be5748f586e209a2929e0209da4c280674546a56
wata-nana <tonarinotototro@gmail.com>
2025-08-21 13:04:41 +0900

    B-3修正

--- B-3/beautiful_kuku.py
+++ B-3/beautiful_kuku.py
@@ -1,10 +1,6 @@
 # 関数の定義
 def beauty_kuku(row_num, column_num):
 
-    # 色付け箇所の色指定
-    RED = "\033[31m"
-    END = "\033[0m"
-
     # 空白桁の設定
     width = len(str(row_num * column_num))
 
@@ -12,7 +8,7 @@ def beauty_kuku(row_num, column_num):
         for j in range(1, column_num + 1):
 
             # 中央揃えかつ桁に応じて空白を設けて表示するよう設定
-            answer = f"{j:<{width}}x {i:<{width}}= {i*j:>{width}} " + RED + "|" + END
+            answer = f"{j:<{width}}x {i:<{width}}= {i*j:>{width}} " + "|"
 
             # 改行 or 空白に分けて出力
             if j == column_num:
